553bb2c994d7c2e31e1e0dbcce8306ce5e2c9db4
MattMucc <mattmucciacciaro118@gmail.com>
2026-08-31 13:23:05 -0400

    Routing
    - Added a pages folder to the src folder.
    - Added Home.jsx to pages folder.
    - Added Books.jsx to pages folder.
    - Updated App.js to import routing and used it.
    - Updated Nav.jsx component to be able to open and close the hamburger menu.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
-import Featured from "./components/Featured";
 import Footer from "./components/Footer";
-import Highlights from "./components/Highlights";
-import Landing from "./components/Landing";
 import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import Books from "./pages/Books"
+import { books } from "./data"
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} /> {/*'path=' is how you create a page path*/}
+        <Route path="/books" render={() => <Books books={books}/>} /> {/*render and the arrow function allows you to pass in props*/}
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
