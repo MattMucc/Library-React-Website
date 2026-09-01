cfc6c406659a5b3fb6b86c074a96a500b1144665
MattMucc <mattmucciacciaro118@gmail.com>
2026-09-01 13:31:12 -0400

    Cart Complete
    - Added a Cart.jsx in the pages folder.
    - Updated App.js to include the new Cart route.
    - Updated the Book.jsx component to add a <Link> to the book.
    - Updated the BookInfo.jsx page to display recommended books and make it dynamic.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,38 @@ import Home from "./pages/Home"
 import Books from "./pages/Books"
 import { books } from "./data"
 import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import React, { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState({});
+  function addToCart() {
+    setCart([...cart, {...books, quantity: 1}])
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart((cart.map(item => {
+      if (+item.id === +book.id) {
+        return {...item, quantity: +quantity}
+      }
+      else {
+        return item
+      }
+    })))
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+  }
+
   return (
     <Router>
       <div className="App">
         <Nav />
         <Route path="/" exact component={Home} /> {/*'path=' is how you create a page path*/}
         <Route path="/books" exact render={() => <Books books={books}/>} /> {/*render and the arrow function allows you to pass in props*/}
-        <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} />} />
+        <Route path="/cart" render={() => <Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem} />} /> {/*render and the arrow function allows you to pass in props*/}
         <Footer />
       </div>
     </Router>
